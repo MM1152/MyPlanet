0bb9318cffa2e66e5db52c7425f87dd2d5a78d5f
천민성 <albert6306@naver.com>
2025-11-06 00:01:43 +0900

    feat: Firebase 통합 시스템 및 Database 기능 구현
    • Firebase Realtime Database와 Auth 기본 구조 구현
    • UniTask 기반 비동기 CRUD 연산 및 JsonSerialized 베이스 클래스 추가
    • FirebaseManager 싱글톤으로 통합 관리 및 초기화 시스템 구축
    • User 모델과 테스트 코드로 기능 검증 완료

--- Assets/Script/Core/Utils/Utils.cs
+++
@@ -1,9 +0,0 @@
-public static class Utils
-{
-    public static List<string> SplitText(string word , string splitWord = '/') 
-    {
-        List<string> splits;
-
-        if(word.)
-    }
-}
--- Assets/Script/TestCode.cs
+++ Assets/Script/TestCode.cs
@@ -1,11 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+public class User : JsonSerialized
+{
+    public string id;
+    public int value;
+
+    public User(string id , int value)
+    {
+        this.id = id;
+        this.value = value;
+    }
+}
 
 public class TestCode : MonoBehaviour 
 {
+    
     private async void Start()
     {
         await FirebaseManager.Instance.WaitForInitalizedAsync();
+        User user = new User("asd" , 1);
+        bool isSuccess = await FirebaseManager.Instance.Database.PushWirteJsonData<User>("users" , user);
+        Debug.Log(isSuccess);
+        var data = await FirebaseManager.Instance.Database.GetData<User>("users");
+        Debug.Log(data.data.id +  " " + data.data.value);
     }
 }
