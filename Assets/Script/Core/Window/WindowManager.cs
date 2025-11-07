2c2f610464c7f83eebcd640d0b413686e2115d69
천민성 <albert6306@naver.com>
2025-11-07 11:59:47 +0900

    pull Stash Data

---
+++ Assets/Script/Core/Window/WindowManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class WindowManager : MonoBehaviour
+{
+    [SerializeField] private List<Window> windows;
+    [SerializeField] private WindowIds startWindow;
+
+    private Dictionary<int, Window> windowTable = new Dictionary<int, Window>();
+
+    private void Start()
+    {
+        foreach(var window in windows)
+        {
+            window.Init();
+            windowTable.Add(window.WindowId, window);
+        }
+
+        if(windowTable.ContainsKey((int)startWindow)) {
+            windowTable[(int)startWindow].Open();
+        }
+    }
+}
