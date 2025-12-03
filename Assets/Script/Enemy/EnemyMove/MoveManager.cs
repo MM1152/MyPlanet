7f29d5fb3ad3499f59eb955d0e58a68423bb5708
leejaeyoung <kltop1210@gmail.com>
2025-12-03 17:53:48 +0900

    feat: Manager_Func

--- Assets/Script/Enemy/EnemyMove/MoveManager.cs
+++ Assets/Script/Enemy/EnemyMove/MoveManager.cs
@@ -1,22 +1,23 @@
- using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class MoveManager 
 {
-  private static Dictionary<EnemyType, IMove> moveTable = new Dictionary<EnemyType, IMove>()
+  private static Dictionary<EnemyType, Func<IMove>> moveTable = new Dictionary<EnemyType, Func<IMove>>()
     {        
-        { EnemyType.Melee, new SimpleMove() },                
-        { EnemyType.Ranged, new SimpleMove() },
-        { EnemyType.EliteMonster, new BaseElementalMove() },
-        { EnemyType.Boss, new BossSimpleMove() },   
+        { EnemyType.Melee, () => new SimpleMove() },                
+        { EnemyType.Ranged, () => new SimpleMove() },
+        { EnemyType.EliteMonster, () => new BaseElementalMove() },
+        { EnemyType.Boss, () => new BossSimpleMove() },   
     };
 
     public static IMove GetMove(EnemyType Id)
     {
         if (moveTable.ContainsKey(Id))
         {
-            return moveTable[Id];
+            return moveTable[Id]();
         }
-        return moveTable[0];    
+        return moveTable[0]();    
     }
 }
