349c1357bdb71983100acc36d1c52e32c80cd5fb
천민성 <albert6306@naver.com>
2025-12-10 20:56:33 +0900

    feat: #224 #225
    - 도감 창 작업 완료

--- Assets/Script/TitleScene/TitleBookWindow.cs
+++ Assets/Script/TitleScene/TitleBookWindow.cs
@@ -29,13 +29,19 @@ public class TitleBookWindow : Window
     [Header("Books")]
     [SerializeField] private GameObject planetBook;
     [SerializeField] private GameObject towerBook;
+    [SerializeField] private GameObject presetBook;
 
     [Header("References")]
     [SerializeField] private WindowManager windowManager;
+    [SerializeField] private PresetViewer presetViewer;
+    [SerializeField] private Transform presetViewerRoot;
 
     private List<PlanetInfomation> planetInfomationList = new List<PlanetInfomation>();
     private List<TowerInfomation> towerInfomationList = new List<TowerInfomation>();
     private GameObject currentOpenBook;
+
+    private List<PresetViewer> presetViewers = new List<PresetViewer>();
+    private int currentSelectPresetIndex = -1;
 
     public override void Init(WindowManager manager)
     {
@@ -43,9 +49,12 @@ public class TitleBookWindow : Window
         windowId = (int)WindowIds.TitleBookWindow;
         InitPlanetInfoList();
         InitTowerInfoList();
+        InitPresetList();
 
         planetBook.SetActive(false);
         towerBook.SetActive(false);
+        presetBook.SetActive(false);
+        FirebaseManager.Instance.PresetData.OnChangePresetData += ChangePresetData;
 
         towerButton.onClick.AddListener(() =>
         {
@@ -66,6 +75,17 @@ public class TitleBookWindow : Window
             }
 
             currentOpenBook = planetBook;
+            currentOpenBook.SetActive(true);
+        });
+
+        presetButton.onClick.AddListener(() =>
+        {
+            if (currentOpenBook != null)
+            {
+                currentOpenBook.SetActive(false);
+            }
+
+            currentOpenBook = presetBook;
             currentOpenBook.SetActive(true);
         });
 
@@ -135,6 +155,51 @@ public class TitleBookWindow : Window
             var path = string.Format(DataBasePaths.TowerUnlockPathFormating , towerDatas[i].ID);
             FirebaseManager.Instance.Database.AddListner(path, towerInfo.OnUnlockValueChanged);
         }
+    }
+
+    private void InitPresetList()
+    {
+        UpdatePreset();
+    }
+
+    private void ChangeSelectPresetIndex(int changeIdx)
+    {
+
+        if (currentSelectPresetIndex != -1)
+        {
+            presetViewers[currentSelectPresetIndex].UpdateSelectButton(false);
+        }
+        currentSelectPresetIndex = changeIdx;
+        presetViewers[currentSelectPresetIndex].UpdateSelectButton(true);
+    }
+
+    private void UpdatePreset()
+    {
+        for (int i = 0; i < presetViewers.Count; i++)
+        {
+            Destroy(presetViewers[i].gameObject);
+        }
+        presetViewers.Clear();
+
+        for (int i = 0; i < FirebaseManager.Instance.PresetData.Count(); i++)
+        {
+            var presetViewer = Instantiate(this.presetViewer, presetViewerRoot);
+            presetViewer.Init(FirebaseManager.Instance.PresetData.Get(i), i, manager, ChangeSelectPresetIndex);
+            presetViewer.CurrentWindowId = (WindowIds)windowId;
+            presetViewers.Add(presetViewer);
+        }
+    }
+
+    private void ChangePresetData(int index)
+    {
+        Debug.Log("Preset ChangeData Call");
+        var presetData = FirebaseManager.Instance.PresetData.Get(index);
+        presetViewers[index].UpdatePreset(presetData);
+    }
+
+    private void OnDestroy()
+    {
+        FirebaseManager.Instance.PresetData.OnChangePresetData -= ChangePresetData;
     }
 
     private void OpenBookInfomationWindow(PlanetTable.Data planetData , PlanetInfomation planetInfo)
