541e0364f1ebdcc68cdef2768a12e2693282c7c4
천민성 <albert6306@naver.com>
2025-12-24 22:20:29 +0900

    feat : 출석체크 작업

---
+++ Assets/Script/Event/DailyGiftLayout.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DailyGiftLayout : MonoBehaviour
+{
+    [SerializeField] private Image giftImage;
+    [SerializeField] private TextMeshProUGUI dayText;
+    [SerializeField] private TextMeshProUGUI valueText;
+
+    public void SetGiftData()
+    {
+
+    }
+}
