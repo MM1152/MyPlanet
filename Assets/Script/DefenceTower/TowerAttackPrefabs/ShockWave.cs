f5237deb548d6e69c196d2a4da9fc22ed290f3f3
leejaeyoung <kltop1210@gmail.com>
2026-01-06 09:57:56 +0900

    feat: shockWave_fix

--- Assets/Script/DefenceTower/TowerAttackPrefabs/ShockWave.cs
+++ Assets/Script/DefenceTower/TowerAttackPrefabs/ShockWave.cs
@@ -7,7 +7,7 @@ public class ShockWave : BaseAttackPrefab
     private TowerTable.UtilTower utilTower;
     private Transform followTarget;
 
-    public float sizeIncreaseValue; 
+    public float sizeIncreaseValue;
     public float durationTimer;
     public Vector3 targetSize;
 
@@ -19,12 +19,12 @@ public class ShockWave : BaseAttackPrefab
 
         duration = data.BonusDuration;
         transform.localScale = Vector3.zero;
-        durationTimer = duration; 
+        durationTimer = duration;
         targetSize = new Vector3(FullRange, FullRange, 1f);
         sizeIncreaseValue = utilTower.range / duration;
         poolsId = PoolsId.ShockWave;
     }
-    
+
     public void SetFollowTarget(Transform followTarget)
     {
         this.followTarget = followTarget;
@@ -34,26 +34,45 @@ public class ShockWave : BaseAttackPrefab
     {
         duration -= Time.deltaTime;
 
-        transform.position = followTarget.position;
+        if (followTarget != null)
+        {
+            transform.position = followTarget.position;
+        }
+        
         transform.localScale = new Vector3(
             Mathf.Min(transform.localScale.x + sizeIncreaseValue * Time.deltaTime, targetSize.x),
             Mathf.Min(transform.localScale.y + sizeIncreaseValue * Time.deltaTime, targetSize.y),
             1f);
 
-        if (duration <= 0) 
+        if (duration <= 0)
         {
             if (gameObject.activeSelf)
                 Managers.ObjectPoolManager.Despawn(poolsId, gameObject);
         }
     }
 
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            HitTarget(collision);
+        }
+    }
+
     protected override void HitTarget(Collider2D collision)
     {
-        var find = collision.GetComponent<IDamageAble>();
-        if(find != null)
+        var enemy = collision.GetComponent<Enemy>();
+        if (enemy == null && collision.attachedRigidbody != null)
         {
-            // 여기서 DeepCopy 로 범위 내 모든 적들이 하나의 effect 효과를 가져서 오류 생김
-            find.StatusEffect.Apply(new StunStatusEffect(tower.BonusDuration) , find);
+            enemy = collision.attachedRigidbody.GetComponentInParent<Enemy>();
+        }
+        if (enemy == null) return;
+
+        var find = enemy.GetComponent<IDamageAble>();
+        if (find != null && find.StatusEffect != null)
+        {
+            // 여기서 DeepCopy 로 범위 내 모든 적들이 하나의 effect 효과를 가져서 오류 생깁
+            find.StatusEffect.Apply(new StunStatusEffect(tower.BonusDuration), find);
         }
     }
 }
